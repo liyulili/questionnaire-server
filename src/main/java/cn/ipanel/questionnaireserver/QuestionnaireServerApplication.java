6b50b4c300a536d0f8d429b3d30198315a847b3a
liyulili <48463783+liyulili@users.noreply.github.com>
2020-02-20 09:25:21 +0800

    工程搭建

---
+++ src/main/java/cn/ipanel/questionnaireserver/QuestionnaireServerApplication.java
@@ -0,0 +1,13 @@
+package cn.ipanel.questionnaireserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnaireServerApplication.class, args);
+    }
+
+}
